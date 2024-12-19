ce8f29e1ec598ae74ee3fb62c55f924fe3333182
Rezakoushki95 <41579107+Rezakoushki95@users.noreply.github.com>
2024-12-19 16:34:47 +0100

    configure server url to run in xcode simulator

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.wedge.apple',
   appName: 'Wedge',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    url: 'http://192.168.1.11:8100',
+    cleartext: true
+  }
 };
 
 export default config;
