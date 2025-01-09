9dfffb96d1fd8861b6acf956ab389b5f3f51614b
Rezakoushki95 <41579107+Rezakoushki95@users.noreply.github.com>
2025-01-09 01:45:09 +0100

    create login module with login and logout functions

--- frontend/src/app/pages/login/login.page.ts
+++ frontend/src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  username = '';
+  password = '';
+  errorMessage = '';
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onLogin() {
+    if (!this.username || !this.password) {
+      this.errorMessage = 'Username and password are required';
+      return;
+    }
+
+    this.authService.login(this.username, this.password).subscribe({
+      next: (response) => {
+        console.log(response.userId)
+        localStorage.setItem('UserId', response.userId.toString()); // Save UserId safely
+        this.router.navigate(['/home']); // Redirect to home
+      },
+      error: (error) => {
+        this.errorMessage = error.message; // Display error message
+      },
+    });
+  }
+
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
   }
 
 }
